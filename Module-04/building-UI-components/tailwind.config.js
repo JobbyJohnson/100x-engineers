b94c8cf4f93c0c36c9e440078580e4e2342b0458
thejobbyjohnson <jobbyjohnson1998@gmail.com>
2023-10-15 21:39:43 +0530

    Adding Components

---
+++ Module-04/building-UI-components/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'Twitter blue- default': '#1D9BF0',
+      'Twitter blue- hover': '#1871CA',
+      'Twitter blue- disabled': '#1E5D87',
+      'Blue wash': '#75BEEF',
+      'Button stroke': '#546A7A',
+      'Searchbar fill': '#212327',
+      'Card fill': '#16181C',
+      'Success': '#00BE74',
+      'Error': '#8B141A',
+      'Neutral/50': '#F9F9F9',
+      'Neutral/100': '#F4F4F4',
+      'Neutral/200': '#E4E4E4',
+      'Neutral/300': '#D3D3D3',
+      'Neutral/400': '#A2A2A2',
+      'Neutral/500': '#737373',
+      'Neutral/600': '#525252',
+      'Neutral/700': '#404040',
+      'Neutral/800': '#262626',
+      'Neutral/900': '#171717',
+      'Neutral/950': '#0A0A0A',
+      'Neutral/1000': '#000000',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
